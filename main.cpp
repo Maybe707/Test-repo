e2dc6ae0c83ed5763ef1b8d2b114188dc60562d3
cyberdemon <fellfrostqtw@gmail.com>
2023-07-15 12:59:21 +0300

    added function for sum numbers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
+int func(int value1, int value2) {
+	return value1 + value2;
+}
+
 int main(int arvc, char* argv[])
 {
 	int value1 = 10;
 	int value2 = 20;
 
-	std::cout << value1 + value2 << std::endl;
+	std::cout << func(value1, value2) << std::endl;
 
 	return 0;
 }
